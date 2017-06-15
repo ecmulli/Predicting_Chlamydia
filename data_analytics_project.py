c2d11552622a901661041cdf6adbe6a45d7bebe4
ecmulli <evancmullins@gmail.com>
2017-06-15 11:19:52 -0400

    Add files via upload

---
+++ data_analytics_project.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 14 10:16:38 2017
+
+@author: evanc
+"""
+
+import pandas as pd
+
+df = pd.read_csv("C:\\Users\\evanc\\Python\\Data Analytics Project\\data.csv")
+
+def process_categorical():
+    global df
+    
+    categories = ["Gender",
+    "Ethnicitiy",
+    "Citizen_of_US",
+    "Education2",
+    "Language",
+    "TestedForHIV",
+    "EverSmokedWeed",
+    "EverHardDrug",
+    "EverUseCocaine",
+    "EverInjectDrugNeedle",
+    "BeenToRehab"
+    ,"CoveredByHealthCare"
+    ,'CoveredByPrivate'
+    ,'CoveredByPublicHC'
+    ,'EverHadHepB'
+    ,'EverHadHepC'
+    ,'GenealHealthCondition'
+    ,'Fem12mosUnablePregnant'
+    ,'FemEverHadReproductiveInfect'
+    ,'FemEverBeenPregn'
+    ,'FemCurrentlyPregnant'
+    ,'BothEverHadSex'
+    ,'MalesEverHadVaginalSex'
+    ,'MalesEverOralSexWithFemale'
+    ,'MalesEverHadAnalSexFemales'
+    ,'MalesEverHadAnalSexMales'
+    ,'FemalesEverHadSexWithMan'
+    ,'FemalesEverHadOralSex'
+    ,'FemalesEverHadAnalSex'
+    ,'FemalesEverHadSexWithFemale'
+    ,'FemalesEverHadHPV'
+    ,'BothEverHadHerpes'
+    ,'BothEverHadGenitalWarts'
+    ,'BothEverHadGono'
+    ,'BothEverHadChl'
+    ,'SexualOrientation']
+    
+    for i in range(len(categories)):
+        category = categories[i]
+        df[category] = df[category].astype('category')
+       
+def train_and_target():
+    global target
+    global df
+    global train
+    df.set_index("SEQN", inplace = True)
+    target = df.ChlamydiaResults
+    df.drop("ChlamydiaResults", axis = 1, inplace = True)
+    train = df
+    train = pd.get_dummies(train)
+
+
+def important_features():
+    global features
+    features = pd.DataFrame()
+    
+    from sklearn.ensemble import ExtraTreesClassifier
+    #from sklearn.feature_selection import SelectFromModel
+    clf = ExtraTreesClassifier(n_estimators=200)
+    clf = clf.fit(train, target)
+    
+    features['features'] = train.columns
+    features['importance'] = clf.feature_importances_
+    features = features.sort(['importance'], ascending = False)
+    
+def run_svm():
+    weights = {0:67.42, 1:892.54}
+    from sklearn import svm
+    from sklearn.model_selection import cross_val_score
+    sv = svm.SVC(C=3, class_weight = weights)
+    clf = sv.fit(train,target)
+    scores = cross_val_score(clf,train,target,cv=10)
+    print(scores)
+
+def run_decision_tree():
+    from sklearn import tree
+    from sklearn.model_selection import cross_val_score
+    tree = tree.DecisionTreeClassifier(class_weight ='balanced')
+    clf = tree.fit(train,target)
+    scores = cross_val_score(clf,train,target,cv =10)
+    print(scores)
+    
+if __name__ == "__main__":
+    process_categorical()
+    train_and_target()
+    important_features()
+    run_svm()
+    run_decision_tree()
